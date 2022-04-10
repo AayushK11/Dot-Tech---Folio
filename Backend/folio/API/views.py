4ecc0062fb34eea01495d97cfce97e0f1f0bceba
Ajinkya R <ajinkyarajkar@gmail.com>
2022-04-10 11:14:23 +0530

    all views done

--- Backend/folio/API/views.py
+++ Backend/folio/API/views.py
@@ -1,4 +1,3 @@
-# Create your views here.
 import API.stock_details
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -45,54 +44,60 @@ def login(request):
             user.invested_value = 0.0
 
             for i in range(len(portfolio)):
-                (Day, Week, Month, Last, Date) = Model.auto_train.auto_train(
-                    portfolio[i].split(",")[0]
-                )
 
-                dict_main["Symbol"] = portfolio[i].split(",")[0]
-                dict_main["Quantity"] = portfolio[i].split(",")[1]
-                dict_main["Invested"] = round(
-                    float(portfolio[i].split(",")[2])
-                    * float(portfolio[i].split(",")[1]),
-                    2,
-                )
+                if portfolio[i] != "":
+                    (Day, Week, Month, Last, Date) = Model.auto_train.auto_train(
+                        portfolio[i].split(",")[0]
+                    )
 
-                dict_main["Company"] = API.stock_details.return_company(
-                    portfolio[i].split(",")[0]
-                )
-                dict_main["Logo"] = API.stock_details.return_logo(
-                    portfolio[i].split(",")[0]
-                )
+                    dict_main["Symbol"] = portfolio[i].split(",")[0]
+                    dict_main["Quantity"] = portfolio[i].split(",")[1]
+                    dict_main["Invested"] = round(
+                        float(portfolio[i].split(",")[2])
+                        * float(portfolio[i].split(",")[1]),
+                        2,
+                    )
 
-                dict_main["LastClose"] = Last
-                dict_main["Gain"] = round(
-                    (float(dict_main["LastClose"]) - float(portfolio[i].split(",")[2]))
-                    * int(portfolio[i].split(",")[1]),
-                    2,
-                )
-                dict_main["Change"] = round(
-                    (dict_main["Gain"] / dict_main["Invested"]) * 100, 2
-                )
+                    dict_main["Company"] = API.stock_details.return_company(
+                        portfolio[i].split(",")[0]
+                    )
+                    dict_main["Logo"] = API.stock_details.return_logo(
+                        portfolio[i].split(",")[0]
+                    )
 
-                user.portfolio_value += round(
-                    float(Last) * int(portfolio[i].split(",")[1]), 2
-                )
-                user.day_projected_value += round(
-                    float(Day) * int(portfolio[i].split(",")[1]), 2
-                )
-                user.week_projected_value += round(
-                    float(Week) * int(portfolio[i].split(",")[1]), 2
-                )
-                user.month_projected_value += round(
-                    float(Month) * int(portfolio[i].split(",")[1]), 2
-                )
-                user.invested_value += round(
-                    float(portfolio[i].split(",")[2]) * int(portfolio[i].split(",")[1]),
-                    2,
-                )
+                    dict_main["LastClose"] = Last
+                    dict_main["Gain"] = round(
+                        (
+                            float(dict_main["LastClose"])
+                            - float(portfolio[i].split(",")[2])
+                        )
+                        * int(portfolio[i].split(",")[1]),
+                        2,
+                    )
+                    dict_main["Change"] = round(
+                        (dict_main["Gain"] / dict_main["Invested"]) * 100, 2
+                    )
 
-                arr_main.append(dict_main)
-                dict_main = {}
+                    user.portfolio_value += round(
+                        float(Last) * int(portfolio[i].split(",")[1]), 2
+                    )
+                    user.day_projected_value += round(
+                        float(Day) * int(portfolio[i].split(",")[1]), 2
+                    )
+                    user.week_projected_value += round(
+                        float(Week) * int(portfolio[i].split(",")[1]), 2
+                    )
+                    user.month_projected_value += round(
+                        float(Month) * int(portfolio[i].split(",")[1]), 2
+                    )
+                    user.invested_value += round(
+                        float(portfolio[i].split(",")[2])
+                        * int(portfolio[i].split(",")[1]),
+                        2,
+                    )
+
+                    arr_main.append(dict_main)
+                    dict_main = {}
 
             change = 0.0
             try:
@@ -175,7 +180,7 @@ def search(request):
 
     else:
         try:
-            stock = request.data["stock"]
+            stock = request.data["stockCode"]
 
             if stock in API.stock_details.return_symbols():
                 email = request.data["email"]
@@ -234,12 +239,12 @@ def search(request):
 
                 return Response(
                     {
-                        "message": "Found",
+                        "message": "Success",
                         "stock": stock,
                         "company": company,
                         "logo": logo,
                         "quantity": quantity,
-                        "buyingprice": buying_price,
+                        "buyingprice": str(round(float(buying_price), 2)),
                         "Change": Change,
                         "Day": Day,
                         "DayChange": DayChange,
@@ -256,3 +261,99 @@ def search(request):
                 return Response({"message": "Stock Does Not Exist"})
         except IndexError:
             return Response({"message": "Stock Does Not Exist"})
+
+
+@csrf_exempt
+@api_view(["POST"])
+def addstock(request):
+    """
+    This function adds a stock to user portfolio.
+    """
+    try:
+        quantity = int(request.data["quantity"])
+        stock = request.data["stock"]
+        buying_price = float(request.data["buyingprice"])
+
+        user = User.objects.get(email=request.data["email"])
+
+        portfolio = user.portfolio.split(";")
+
+        for i in portfolio:
+            if i.split(",")[0] == stock:
+                portfolio_temp = user.portfolio.split(";")
+                portfolio_temp.remove("")
+
+                for i in range(len(portfolio_temp)):
+                    if portfolio_temp[i].split(",")[0] == stock:
+
+                        quantity_update = int(portfolio_temp[i].split(",")[1]) + int(
+                            quantity
+                        )
+                        old_value = float(portfolio_temp[i].split(",")[2]) * int(
+                            portfolio_temp[i].split(",")[1]
+                        )
+                        new_value = float(buying_price) * int(quantity)
+                        new_average = (old_value + new_value) / quantity_update
+
+                        portfolio_temp[i] = (
+                            stock
+                            + ","
+                            + str(quantity_update)
+                            + ","
+                            + str(new_average)
+                            + ";"
+                        )
+
+                        user.portfolio = ";".join(portfolio_temp)
+
+                        user.save()
+
+                        return Response({"message": "Added to Portfolio"})
+
+        else:
+            if stock in API.stock_details.return_symbols():
+
+                user.portfolio += (
+                    str(stock) + "," + str(quantity) + "," + str(buying_price) + ";"
+                )
+                user.save()
+
+                return Response({"message": "Added to Portfolio"})
+
+            else:
+                return Response({"message": "Stock Does Not Exist"})
+
+    except User.DoesNotExist:
+        return Response({"message": "User Does Not Exist"})
+
+
+@csrf_exempt
+@api_view(["POST"])
+def removestock(request):
+    """
+    This function removes a stock from user portfolio.
+    """
+    try:
+        stock = request.data["stock"]
+        email = request.data["email"]
+
+        user = User.objects.get(email=email)
+
+        portfolio_temp = user.portfolio.split(";")
+        portfolio_temp.remove("")
+
+        for i in range(len(portfolio_temp)):
+            if portfolio_temp[i].split(",")[0] == stock:
+
+                portfolio_temp[i] = ""
+
+        user.portfolio = ";".join(portfolio_temp)
+        user.save()
+
+        return Response({"message": "Removed from Portfolio"})
+
+    except User.DoesNotExist:
+        return Response({"message": "User Does Not Exist"})
+
+
+# Model.auto_train.auto_train()
